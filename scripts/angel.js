17a1eef2bc9d88ef31f3e1a11d7ca675db5440ef
Marcin Zawadzki <thelon78@gmail.com>
2013-02-10 18:33:46 +0100

    start screen

--- scripts/angel.js
+++ scripts/angel.js
@@ -1,5 +1,6 @@
-/*
-{function(){
+console.log('loaded angel.js');
+
+(function(){
 // UTIL CLASS /
 var Util = {
 	init : function initArray(){
@@ -7,6 +8,22 @@ var Util = {
 		for (var i = 0; i < this.length; i++)
 			this[i+1] = initArray.arguments[i]
 	}
+}
+
+var Const = {
+    CanvasWidth : 400,
+	CanvasHeight : 300
+}
+
+var Font = {
+	Title : "normal 32px Monotype",
+	Subtitle : "normal 20px Monotype",
+    Subtitle2 : "normal 12px Monotype"
+}
+
+var Colors = {
+    Pretty : "lightBlue",
+	Background : "blue"
 }
 
 var ResPL ={
@@ -18,8 +35,8 @@ var ResPL ={
              "Nie zabijaj.",
              "Nie cudzołóż.",
              "Nie kradnij.",
-             "Nie mów fałszywego świadectwa przeciw bliżniemu swemu."
-             "Nie pożądaj żony bliźniego swego."
+             "Nie mów fałszywego świadectwa przeciw bliżniemu swemu.",
+             "Nie pożądaj żony bliźniego swego.",
              "Ani żadnej rzeczy , która jego jest."],
 	BlessingsPart1:["Błogosławieni ubodzy w duchu",
 					"Błogosławieni, którzy się smucą",
@@ -39,12 +56,31 @@ var ResPL ={
 					", albowiem do nich należy Królestwo niebieskie."]	
 };
 
+/* CANVAS VIEW */
+function viewStart()
+{
+	context.fillStyle = Font.Background;
+	context.fillRect(0,0, Const.CanvasWidth ,Const.CanvasHeight);
+
+	context.font      = Font.Title;
+	context.fillStyle = Colors.Pretty;
+	context.fillText("Anioł Stróż", 120, 70);
+	context.font      = Font.Subtitle;
+	context.fillText("epizod 1", 140, 180);
+	context.font      = Font.Subtitle2;
+	context.fillText("Marcin Zawadzki (thelon78@gmail.com)", 140, 200);
+}
+
 var Res = ResPL;
+var context;
 
 function Init(){
+	var canvas  = document.getElementById("canvas");
+	context = canvas.getContext("2d");
+	viewStart();
+	console.log('Initialized');
 };
 
 Init();
-}();
-*/
-}}();
+})();
+

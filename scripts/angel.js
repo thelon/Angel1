18ada03af5225129788ed2476cdbb7851343f769
Marcin Zawadzki <thelon78@gmail.com>
2013-02-10 15:34:26 +0100

    Media start

---
+++ scripts/angel.js
@@ -0,0 +1,50 @@
+/*
+{function(){
+// UTIL CLASS /
+var Util = {
+	init : function initArray(){
+		this.length = initArray.arguments.length
+		for (var i = 0; i < this.length; i++)
+			this[i+1] = initArray.arguments[i]
+	}
+}
+
+var ResPL ={
+	Rules : ["Jam jest Pan .Bóg twój, który cie wywiódł z ziemi egipskiej z domu niewoli.",
+			 "Nie będziesz miał bogów cudzych przede Mną.",
+			 "Nie będziesz brał imienia Pana Boga swego na daremno.",
+			 "Pamiętaj , abyś dzień świety święcił.",
+			 "Czcij ojca swego i matke swoją.",
+             "Nie zabijaj.",
+             "Nie cudzołóż.",
+             "Nie kradnij.",
+             "Nie mów fałszywego świadectwa przeciw bliżniemu swemu."
+             "Nie pożądaj żony bliźniego swego."
+             "Ani żadnej rzeczy , która jego jest."],
+	BlessingsPart1:["Błogosławieni ubodzy w duchu",
+					"Błogosławieni, którzy się smucą",
+                    "Błogosławieni cisi",
+					"Błogosławieni, którzy łakną i pragną sprawiedliwości",
+					"Błogosławieni miłosierni",
+					"Błogosławieni czystego serca",		
+					"Błogosławieni, którzy wprowadzają pokój",
+					"Błogosławieni, którzy cierpią prześladowanie dla sprawiedliwości"],
+	BlessingsPart2:[", albowiem do nich należy Królestwo niebieskie.",
+					", albowiem oni będą pocieszeni.",
+					", albowiem oni na własność posiądą ziemię.",
+					", albowiem oni będą nasyceni.",
+					", albowiem oni miłosierdzia dostąpią.",
+					", albowiem oni Boga oglądać będą.",
+					", albowiem oni będą nazwani synami Bożymi.",
+					", albowiem do nich należy Królestwo niebieskie."]	
+};
+
+var Res = ResPL;
+
+function Init(){
+};
+
+Init();
+}();
+*/
+}}();
